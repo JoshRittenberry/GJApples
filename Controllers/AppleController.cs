9420e4f16012ca5ac12d182c57498c66918e52e9
Josh Rittenberry <141363044+JoshRittenberry@users.noreply.github.com>
2024-01-22 13:26:14 -0600

    Get AppleVariety By Id Completed

--- Controllers/AppleController.cs
+++ Controllers/AppleController.cs
@@ -65,4 +65,57 @@ public class AppleController : ControllerBase
             }).ToList()
         );
     }
+
+    [HttpGet("{id}")]
+    [Authorize]
+    public IActionResult Get(int id)
+    {
+        var appleVariety = _dbContext
+            .AppleVarieties
+                .Include(a => a.Trees)
+                    .ThenInclude(t => t.TreeHarvestReports)
+                .Include(a => a.OrderItems)
+            .SingleOrDefault(a => a.Id == id);
+
+        if (appleVariety == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(new AppleVarietyDTO
+        {
+            Id = appleVariety.Id,
+            Type = appleVariety.Type,
+            ImageUrl = appleVariety.ImageUrl,
+            CostPerPound = appleVariety.CostPerPound,
+            Trees = appleVariety.Trees
+                    .Where(t => t.DateRemoved == null)
+                    .Select(t => new TreeDTO
+                    {
+                        Id = t.Id,
+                        AppleVarietyId = t.AppleVarietyId,
+                        AppleVariety = null,
+                        DatePlanted = t.DatePlanted,
+                        DateRemoved = t.DateRemoved,
+                        TreeHarvestReports = t.TreeHarvestReports.Select(thr => new TreeHarvestReportDTO
+                        {
+                            Id = thr.Id,
+                            TreeId = thr.TreeId,
+                            Tree = null,
+                            EmployeeUserProfileId = thr.EmployeeUserProfileId,
+                            Employee = null,
+                            HarvestDate = thr.HarvestDate,
+                            PoundsHarvested = thr.PoundsHarvested
+                        }).ToList()
+                    }).ToList(),
+            OrderItems = appleVariety.OrderItems.Select(oi => new OrderItemDTO
+            {
+                Id = oi.Id,
+                OrderId = oi.OrderId,
+                AppleVarietyId = oi.AppleVarietyId,
+                AppleVariety = null,
+                Pounds = oi.Pounds
+            }).ToList()
+        });
+    }
 }
