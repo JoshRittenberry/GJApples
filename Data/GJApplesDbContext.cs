3c218e3c3d16c31bcfe7f18beb32ba76059672d4
Josh Rittenberry <141363044+JoshRittenberry@users.noreply.github.com>
2024-01-22 09:00:43 -0600

    quick save

--- Data/GJApplesDbContext.cs
+++ Data/GJApplesDbContext.cs
@@ -10,8 +10,11 @@ public class GJApplesDbContext : IdentityDbContext<IdentityUser>
     private readonly IConfiguration _configuration;
 
     public DbSet<UserProfile> UserProfiles { get; set; }
-
-    // Define Classes
+    public DbSet<AppleVariety> AppleVarieties { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+    public DbSet<Tree> Trees { get; set; }
+    public DbSet<TreeHarvestReport> TreeHarvestReports { get; set; }
 
     public GJApplesDbContext(DbContextOptions<GJApplesDbContext> context, IConfiguration config) : base(context)
     {
@@ -22,18 +25,39 @@ public class GJApplesDbContext : IdentityDbContext<IdentityUser>
     {
         base.OnModelCreating(modelBuilder);
 
-        modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole
+        modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole[]
         {
-            Id = "c3aaeb97-d2ba-4a53-a521-4eea61e59b35",
-            Name = "Admin",
-            NormalizedName = "admin"
+            new IdentityRole
+            {
+                Id = "c3aaeb97-d2ba-4a53-a521-4eea61e59b35",
+                Name = "Admin",
+                NormalizedName = "admin"
+            },
+            new IdentityRole
+            {
+                Id = "8b2b3a2d-62f6-4f2b-8b3d-45b6a1f3b5b4",
+                Name = "Harvester",
+                NormalizedName = "harvester"
+            },
+            new IdentityRole
+            {
+                Id = "f65f1f30-d0b1-4f59-a3c8-eb1f2e6757d3",
+                Name = "OrderPicker",
+                NormalizedName = "orderpicker"
+            },
+            new IdentityRole
+            {
+                Id = "d4f146bf-70c8-4d02-98ec-0b5f4b9d213f",
+                Name = "Customer",
+                NormalizedName = "customer"
+            }
         });
 
         modelBuilder.Entity<IdentityUser>().HasData(new IdentityUser
         {
             Id = "dbc40bc6-0829-4ac5-a3ed-180f5e916a5f",
             UserName = "Administrator",
-            Email = "admina@strator.comx",
+            Email = "admin@gjapples.com",
             PasswordHash = new PasswordHasher<IdentityUser>().HashPassword(null, _configuration["AdminPassword"])
         });
 
