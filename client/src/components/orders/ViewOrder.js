7b2bba631921d4d5c32bb769a23d0e0ac94e5427
Josh Rittenberry <141363044+JoshRittenberry@users.noreply.github.com>
2024-01-26 13:10:13 -0600

    view order page styled

--- client/src/components/orders/ViewOrder.js
+++ client/src/components/orders/ViewOrder.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import "../stylesheets/viewOrder.css"
 import { useNavigate, useParams } from "react-router-dom"
 import { cancelOrder, getOrderById } from "../../managers/orderManager"
 import { Button, Table } from "reactstrap"
@@ -17,28 +18,12 @@ export const ViewOrder = ({ loggedInUser }) => {
     return (
         <>
             <header className="vieworder_header">
-                <h1>View Order</h1>
                 <h3>Order #{order.id}</h3>
-                <h3>Customer Id #{order.customerUserProfileId}</h3>
-                <h5>Phone: (XXX)-XXX-XXXX</h5>
-                <h5>Email: xxx@xxxx.com</h5>
-                {!order.canceled && order.employeeUserProfileId === null && order.dateCompleted === null && (
-                    <>
-                        <Button onClick={() => {
-                            cancelOrder(order.id).then(() => {
-                                getOrderById(orderId).then(setOrder)
-                                navigate("/orderhistory")
-                            })
-                        }}>
-                            Cancel Order
-                        </Button>
-                        <Button onClick={() => {
-                            navigate(`/orderhistory/edit/${order.id}`)
-                        }}>
-                            Edit Order
-                        </Button>
-                    </>
-                )}
+                <h5>Customer #{order.customerUserProfileId}</h5>
+                <div className="vieworder_header_more">
+                    <h5>Phone: (XXX)-XXX-XXXX</h5>
+                    <h5>Email: xxx@xxxx.com</h5>
+                </div>
             </header>
             <section className="vieworder_body">
                 <Table>
@@ -55,7 +40,7 @@ export const ViewOrder = ({ loggedInUser }) => {
                             <tr key={`orderitem-${oi.id}`}>
                                 <th
                                     scope="row"
-                                >
+                                    >
                                     {oi.appleVariety?.type}
                                 </th>
                                 <th>{oi.pounds} lbs</th>
@@ -73,6 +58,23 @@ export const ViewOrder = ({ loggedInUser }) => {
                         </tr>
                     </tbody>
                 </Table>
+                {!order.canceled && order.employeeUserProfileId === null && order.dateCompleted === null && (
+                    <div className="vieworder_body_buttons">
+                        <Button onClick={() => {
+                            cancelOrder(order.id).then(() => {
+                                getOrderById(orderId).then(setOrder)
+                                navigate("/orderhistory")
+                            })
+                        }}>
+                            Cancel Order
+                        </Button>
+                        <Button onClick={() => {
+                            navigate(`/orderhistory/edit/${order.id}`)
+                        }}>
+                            Edit Order
+                        </Button>
+                    </div>
+                )}
             </section>
             <ContactUsFooter />
         </>
