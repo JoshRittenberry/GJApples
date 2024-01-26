4445a5fcf7d438d55d501fcbe8de4a5781d488aa
Josh Rittenberry <141363044+JoshRittenberry@users.noreply.github.com>
2024-01-26 14:11:43 -0600

    footer styled

--- client/src/components/ContactUsFooter.js
+++ client/src/components/ContactUsFooter.js
@@ -1,15 +1,20 @@
+import "./stylesheets/contactUsFooter.css"
+
 export const ContactUsFooter = () => {
     return (
         <footer className="contactusfooter">
             <h3>Contact Us</h3>
             <div className="contactusfooter_address">
-                <p>2584 Orchard Lane</p>
-                <p>Mount Juliet, TN 37122</p>
+                2584 Orchard Lane
+                <br />
+                Mount Juliet, TN 37122
             </div>
             <div className="contactusfooter_contactinfo">
-                <p>Phone Number: (615) 502-7483</p>
-                <p>Email: contact@garyjonesappleorchard.com</p>
+                Phone Number: (615) 502-7483
+                <br />
+                Email: <a href="mailto:contact@garyjonesappleorchard.com">contact@garyjonesappleorchard.com</a>
             </div>
         </footer>
+
     )
 }
