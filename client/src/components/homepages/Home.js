5b2cca0742a37842b94e97fdceae49b56b376949
Josh Rittenberry <141363044+JoshRittenberry@users.noreply.github.com>
2024-01-27 09:51:38 -0600

    added new item to homepage - GJ's Greeting

--- client/src/components/homepages/Home.js
+++ client/src/components/homepages/Home.js
@@ -3,12 +3,14 @@ import "../stylesheets/home.css"
 import "../../App.css"
 import { HomePageWelcome } from "./HomePageWelcome"
 import { Cards } from "../cards/Cards"
+import { HomePageGJGreeting } from "./HomePageGJGreeting"
 
 export const Home = ({ loggedInUser }) => {
     console.log(loggedInUser)
     return (
         <>
             <HomePageWelcome loggedInUser={loggedInUser} />
+            <HomePageGJGreeting />
             <Cards />
         </>
     )
