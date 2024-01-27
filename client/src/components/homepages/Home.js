8d703793382565c8f3e3ec83f1ab80b3713ded9e
Josh Rittenberry <141363044+JoshRittenberry@users.noreply.github.com>
2024-01-27 09:31:50 -0600

    added cards

--- client/src/components/homepages/Home.js
+++ client/src/components/homepages/Home.js
@@ -2,12 +2,14 @@ import React from "react"
 import "../stylesheets/home.css"
 import "../../App.css"
 import { HomePageWelcome } from "./HomePageWelcome"
+import { Cards } from "../cards/Cards"
 
 export const Home = ({ loggedInUser }) => {
     console.log(loggedInUser)
     return (
         <>
             <HomePageWelcome loggedInUser={loggedInUser} />
+            <Cards />
         </>
     )
 }
