c6fdb6a5479a67fa2527192473b6f174632236b5
Josh Rittenberry <141363044+JoshRittenberry@users.noreply.github.com>
2024-01-27 21:35:54 -0600

    quick save

--- client/src/components/History.js
+++ client/src/components/History.js
@@ -84,7 +84,8 @@ export const History = () => {
     return (
         <>
             <div className='history-container'>
-                {screenWidth > 960 && <video className="history-container-video" src='/videos/hp_apples_v.mp4' autoPlay loop muted />}
+                <video className="history-container-video" src='/videos/hp_apples_v.mp4' playsInline autoPlay loop muted />
+                {/* {screenWidth > 960 && <video className="history-container-video" src='/videos/hp_apples_v.mp4' autoPlay loop muted />} */}
                 <div className="timeline">
                     {timelineElements.map(te => {
                         if (position == "right" || position == "") {
