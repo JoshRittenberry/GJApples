fa35ead1420babb2f157c3618cda64bb5aa91d93
Josh Rittenberry <141363044+JoshRittenberry@users.noreply.github.com>
2024-01-29 11:26:10 -0600

    updated admin home page

--- client/src/components/homepages/AdminHomePage.js
+++ client/src/components/homepages/AdminHomePage.js
@@ -6,9 +6,6 @@ export const AdminHomePage = () => {
     return (
         <>
             <div className="adminhome">
-                <header className="adminhome_header">
-                    <h1>Admin Home Page</h1>
-                </header>
                 <section className="adminhome_body">
                     <AdminSelections />
                 </section>
