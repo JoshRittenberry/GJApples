43a8c406484c06406f7a51e1c46e731579fe6f88
Josh Rittenberry <141363044+JoshRittenberry@users.noreply.github.com>
2024-01-29 18:39:01 -0600

    trying to fix auth on different device

--- client/src/managers/authManager.js
+++ client/src/managers/authManager.js
@@ -3,7 +3,8 @@ const _apiUrl = "/api/Auth";
 export const login = (email, password) => {
     return fetch(_apiUrl + "/login", {
         method: "POST",
-        credentials: "same-origin",
+        // credentials: "same-origin",
+        credentials: "include",
         headers: {
             Authorization: `Basic ${btoa(`${email}:${password}`)}`,
         },
@@ -29,7 +30,8 @@ export const tryGetLoggedInUser = () => {
 export const register = (userProfile) => {
     userProfile.password = btoa(userProfile.password);
     return fetch(_apiUrl + "/register", {
-        credentials: "same-origin",
+        // credentials: "same-origin",
+        credentials: "include",
         method: "POST",
         headers: {
             "Content-Type": "application/json",
