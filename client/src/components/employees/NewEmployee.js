b6632e85469b0f5dd823168bad4e756a20c8548d
Josh Rittenberry <141363044+JoshRittenberry@users.noreply.github.com>
2024-01-31 14:21:12 -0600

    quick save

--- client/src/components/employees/NewEmployee.js
+++ client/src/components/employees/NewEmployee.js
@@ -1,15 +1,98 @@
+import { useState } from "react";
+import { register } from "../../managers/authManager";
+import { Link, useNavigate } from "react-router-dom";
+import { Button, FormFeedback, FormGroup, Input, Label } from "reactstrap";
 import { Footer } from "../Footer"
-import "../stylesheets/newEmployee.css"
+import "../stylesheets/register.css"
 
-export const NewEmployee = () => {
+export const NewEmployee = ({setLoggedInUser}) => {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [userName, setUserName] = useState("");
+    const [email, setEmail] = useState("");
+    const [address, setAddress] = useState("");
+    const [password, setPassword] = useState("");
+
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+
+    };
+
     return (
         <>
-            <div className="newemployee">
-                <header className="newemployee_header">
-                    <h1>New Employee</h1>
-                </header>
+            <div className="container register-container" style={{ maxWidth: "500px" }}>
+                <h3>Create an Employee Account</h3>
+                <FormGroup>
+                    <Label>First Name</Label>
+                    <Input
+                        type="text"
+                        value={firstName}
+                        onChange={(e) => {
+                            setFirstName(e.target.value);
+                        }}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label>Last Name</Label>
+                    <Input
+                        type="text"
+                        value={lastName}
+                        onChange={(e) => {
+                            setLastName(e.target.value);
+                        }}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label>Email</Label>
+                    <Input
+                        type="email"
+                        value={email}
+                        onChange={(e) => {
+                            setEmail(e.target.value);
+                        }}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label>User Name</Label>
+                    <Input
+                        type="text"
+                        value={userName}
+                        onChange={(e) => {
+                            setUserName(e.target.value);
+                        }}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label>Address</Label>
+                    <Input
+                        type="text"
+                        value={address}
+                        onChange={(e) => {
+                            setAddress(e.target.value);
+                        }}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label>Password</Label>
+                    <Input
+                        type="password"
+                        value={password}
+                        onChange={(e) => {
+                            setPassword(e.target.value);
+                        }}
+                    />
+                </FormGroup>
+                <div className="register-container-options">
+                    <Button
+                        color="primary"
+                        onClick={handleSubmit}
+                    >
+                        Create Account
+                    </Button>
+                </div>
             </div>
             <Footer />
         </>
-    )
+    );
 }
